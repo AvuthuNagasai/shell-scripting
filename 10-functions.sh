1aee212b4648b3f4e1ca5bd3d1214bb612405ea5
nagasai <avuthunagasaireddy@gmail.com>
2024-07-02 16:17:11 +0530

    learning devops

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-echo "Hi, i am learning devops"
+#echo "Hi, i am learning devops"
+#
+#Person1=$1
+#Person2=$2
+#
+#echo "$Person1: hi , how are you ?"
+#echo "$Person2: fine, how are you doing?"
+#echo "$Person1:nice to see you!!"
+#echo "$Person2:see you, bye bye!"
 
-Person1=$1
-Person2=$2
 
-echo "$Person1: hi , how are you ?"
-echo "$Person2: fine, how are you doing?"
-echo "$Person1:nice to see you!!"
-echo "$Person2:see you, bye bye!"
+#!/bin/bash
+
+echo "Please enter username::"
+
+read -s USERNAME #here USERNAME is variable
+
+echo "Please enter password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
+
+
 
 
 
