d911a37f7a14c7d68fef4832e1e7ba8911f64703
nagasai <avuthunagasaireddy@gmail.com>
2024-07-02 16:07:44 +0530

    learning devops

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
 echo "Hi, i am learning devops"
+
+echo "Person1: hi , how are you ?"
+echo "Person2: fine, how are you doing?"
+echo "nice to see you!!"
+echo "see you, bye bye!"
+
+
