7226b168677f7ffd6662feae834d64c716decc8a
nagasai <avuthunagasaireddy@gmail.com>
2024-07-02 17:05:37 +0530

    learning devops

--- 03-functions.sh
+++ 03-functions.sh
@@ -1,32 +1,64 @@
+##!/bin/bash
+#
+#TIMESTAMP=$(date +%F-%H-%M-%S)
+#SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+#LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+#
+#USERID=$(id -u)
+#
+#if [ USERID -ne 0 ]
+#then
+#   echo "Please run this script with root access."
+#else
+#   echo "you are super user"
+#fi
+#
+#
+#VALIDATE(){
+#if [ $1 -ne 0 ]
+#then
+#    echo "Installation of mysql...FAILURE"
+#    exit 1
+#else
+#    echo "Installation of mysql...SUCCESS"
+#fi
+#
+#}
+#
+#dnf install mysql -y $>>LOGFILE
+#VALIDATE $? "Installing MySQL"
+#dnf install nginx -y $>>LOGFILE
+#VALIDATE $? "Installing Nginx"
+#
+
+
 #!/bin/bash
 
+USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-USERID=$(id -u)
-
-if [ USERID -ne 0 ]
-then
-   echo "Please run this script with root access."
-else
-   echo "you are super user"
-fi
-
-
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo "Installation of mysql...FAILURE"
-    exit 1
-else
-    echo "Installation of mysql...SUCCESS"
-fi
-
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
 
-dnf install mysql -y $>>LOGFILE
-VALIDATE $? "Installing MySQL"
-dnf install nginx -y $>>LOGFILE
-VALIDATE $? "Installing Nginx"
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
