30d601d20acb3c2edd15c414251a32dcedfa9d10
nagasai <avuthunagasaireddy@gmail.com>
2024-07-03 13:41:55 +0530

    learning devops

---
+++ 04-ipackages-nstallation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME= echo( $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME$TIMESTAMP.log
+
+R= \e[31m
+G= \e[32m
+Y= \e[33m
+
+
+if [ $USERID -ne 0 ]
+then
+  echo "you must rin the code as a super user"
+  exit 1
+else
+  echo "you are a super user"
+fi
+
+for i in $@
+do
+    echo "package to install: $i"
+    dnf list installed $i &>>LOGFILE
+    if[$? -eq o]
+    then
+      echo " $i is already installed"
+    else
+      echo "$i need to be installed"
+      dnf install $i
+    fi
+done
+
